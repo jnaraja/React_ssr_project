94181e48e10c5792ac018683a28acf3d715e178b
Jonathan Naraja <107944482+j12naraja@users.noreply.github.com>
2022-08-26 01:51:14 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './index.module.css';
+import Card from './Card';
+import { initStore, initialCards, addItem } from '../store';
+import { useDispatch } from 'react-redux';
+
+const Index = () => {
+
+    const dispatch = useDispatch();
+    const { cards } = dispatch(initialCards());
+    return (
+        <div className={styles.app}>
+            <header className={styles.header}>
+                <img src="/logo.png"
+                    className={styles.logo} alt="logo"
+                />
+            </header>
+            <div className={styles.grid}>
+                {
+                    cards.map((card) => (
+                        <Card key={card.id} />
+                    ))
+                }
+            </div>
+            {/* <button onClick={() => dispatch(addItem())}></button> */}
+        </div>
+    )
+};
+export default initStore.withRedux(Index);
